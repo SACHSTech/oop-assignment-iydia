30517edd0ea889e54a2a5df45768226bdffd1dda
iydia <lydia.he23@ycdsbk12.ca>
2022-11-12 15:44:34 -0500

    working on member.java

--- src/Online_Store/Member.java
+++ src/Online_Store/Member.java
@@ -1,11 +1,14 @@
 package Online_Store;
 
 public class Member extends User{
+    private int rewardsPoints;
 
     public Member(String user, String pass) {
         super(user, pass);
     }
 
     
+
+
     
 }
