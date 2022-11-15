33ab0ce22872b5be1125264523897da2fcd839b5
iydia <lydia.he23@ycdsbk12.ca>
2022-11-15 09:42:25 -0500

    Starting Cart.java

--- src/Online_Store/Cart.java
+++ src/Online_Store/Cart.java
@@ -1,5 +1,14 @@
 package Online_Store;
 
 public class Cart {
-    
+    // Instance Variables
+    private double total;
+    private boolean placeOrder;
+    private boolean isDelivered;
+    private String orderStatus;
+
+    public Cart(double total, ){
+        // do something like quantity = items.quantity();
+
+    }
 }
