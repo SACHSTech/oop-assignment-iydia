51045441b74c7feeae229e026f9667bf4891f17a
iydia <lydia.he23@ycdsbk12.ca>
2022-11-15 10:04:41 -0500

    Order constructor made

--- src/Online_Store/Order.java
+++ src/Online_Store/Order.java
@@ -1,14 +1,15 @@
 package Online_Store;
 
-public class Cart {
+public class Order {
     // Instance Variables
     private double total;
     private boolean placeOrder;
     private boolean isDelivered;
     private String orderStatus;
 
-    public Cart(double total, ){
+    public Order(double theTotal, String theOrderStatus){
         // do something like quantity = items.quantity();
-
+        total = theTotal;
+        orderStatus = theOrderStatus;
     }
 }
