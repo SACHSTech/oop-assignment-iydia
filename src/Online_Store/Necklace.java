a5319cce37b68e9974465f0d66ccf0776bcc1186
iydia <lydia.he23@ycdsbk12.ca>
2022-11-16 19:58:37 -0500

    necklace.java getter methods

--- src/Online_Store/Necklace.java
+++ src/Online_Store/Necklace.java
@@ -2,16 +2,25 @@ package Online_Store;
 
 public class Necklace extends Items{
     private String size;
+    private String material;
     private int quantity;
     private double price;
 
-    public Necklace(String name, int quantity, double price) {
+    public Necklace(String name, int quantity, double price, String material) {
         super(name);
         this.quantity = quantity;
         this.price = price;
+        this.material = material;
     }
 
     public String getSize() {
+        if (size == "S"){ // NOTE: change later to ignore uppercase/lowercase
+            return "Small: 40cm";
+        } else if (size == "M"){
+            return "Medium: 50cm";
+        } else if (size == "L"){
+            return "Large: 60cm"; // LATER: add else statement
+        }
         return size;
     }
 
@@ -23,4 +32,12 @@ public class Necklace extends Items{
         return price;
     }
     
+    public String getMaterial() {
+        if (material == "1"){
+            return "Silver";
+        } else if (material == "2"){
+            return "Gold";
+        }
+        return material;
+    }
 }
--- src/Online_Store/Rings.java
+++ src/Online_Store/Rings.java
@@ -2,6 +2,8 @@ package Online_Store;
 
 public class Rings extends Items{
 
+    
+
     public Rings(String theName) {
         super(theName);
     }
